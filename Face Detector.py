fe4b489d46c3f355d5fe644571dd7c7192ac308c
Wanjin Maglangit <wmaglangit1118@gmail.com>
2022-11-19 11:13:56 +0800

    added CV2 detect faces

--- Face Detector.py
+++ Face Detector.py
@@ -10,3 +10,10 @@ camera = cv2.VideoCapture(0)
 while True:
     _, frame = camera.read()
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(
+        grey,
+        scaleFactor = 1.1,
+        minNeighbors = 5,
+        minSize = (30,30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
