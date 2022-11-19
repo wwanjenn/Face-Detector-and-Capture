e671bcf15b87535ae148077083b26bc9bf49dce0
Wanjin Maglangit <wmaglangit1118@gmail.com>
2022-11-19 11:05:31 +0800

    added classifier

--- Face Detector.py
+++ Face Detector.py
@@ -2,4 +2,6 @@ import pathlib
 import cv2
 
 cascade_path = pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
-print(cascade_path)
+
+clf = cv2.CascadeClassifier(str(cascade_path))
+
